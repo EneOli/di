69496925bd71963f48983a925a07896b4e4a22d8
Oliver Enes <oliver@enesnet.de>
2021-07-30 22:47:20 +0200

    service decorator

--- decorators/service.ts
+++ decorators/service.ts
@@ -1,5 +1,21 @@
+import { ParameterRegistry } from "../parameter-registry";
+import { Constructor } from "../types/constructor";
+import { ServiceDescriptor } from "../types/service-descriptor";
+import { DI_REFLECT_CUSTOM_INJECTION } from "./inject";
+
 export function Service<T>() {
-  return function (target: T, propertyKey: string, descriptor: PropertyDescriptor) {
-    Reflect.getMetadata();
+  return function (constructor: Constructor<T>) {
+    const paramTypes = Reflect.getMetadata("design:paramtypes", constructor) || [];
+
+    const customInjectionMap: Map<number, ServiceDescriptor<unknown>> = Reflect.getMetadata(DI_REFLECT_CUSTOM_INJECTION, constructor);
+
+
+    if (customInjectionMap) {
+      for (const key of customInjectionMap.keys()) {
+        paramTypes[key] = customInjectionMap.get(key);
+      }
+    }
+
+    ParameterRegistry.set(constructor, paramTypes);
   }
 }
